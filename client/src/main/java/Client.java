6742b38ce26f612c2c36e8e41212f7ff84e28241
Karol Hamielec <karol.hamielec@gmail.com>
2020-11-28 14:16:01 +0100

    Client app added. Available commands:  * add-user <first_name> <last_name> <email>  * show-users

---
+++ client/src/main/java/Client.java
@@ -0,0 +1,171 @@
+import Model.User;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.jakewharton.fliptables.FlipTableConverters;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Command(
+        name = "show-users"
+)
+class ShowUsersCommand implements Runnable {
+    private static final String apiURL = "http://localhost:8080/api/users";
+
+    @Override
+    public void run() {
+        System.out.println("Getting users...");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("accept", "application/json")
+                .uri(URI.create(apiURL))
+                .build();
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            List<User> users = mapper.readValue(response.body(), new TypeReference<List<User>>() {});
+
+            System.out.println(FlipTableConverters.fromIterable(users, User.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+@Command(
+        name = "add-user"
+)
+class AddUserCommand implements Runnable {
+    private static final String apiURL = "http://localhost:8080/api/users";
+
+    @Parameters(index = "0", description = "first name")
+    private String firstName;
+    @Parameters(index = "1", description = "last name")
+    private String lastName;
+    @Parameters(index = "2", description = "email address")
+    private String email;
+    @Override
+    public void run() {
+//        String firstName = "karol";
+//        String lastName = "Hamielec";
+//        String email = "SSSS";
+        System.out.println("Adding user..." + firstName + " " + lastName + " " + email);
+        String request_body = String.format("{\"firstName\": \"%s\", \"lastName\": \"%s\", \"email\": \"%s\"}", firstName, lastName, email);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(request_body))
+                .header("Content-Type", "application/json")
+                .uri(URI.create(apiURL))
+                .build();
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+@Command(
+        name = "add-screening-room"
+)
+class AddScreeningRoomCommand implements Runnable {
+    @Parameters(index = "0", description = "letter")
+    private char letter;
+    @Parameters(index = "1", description = "description")
+    private String description;
+    @Parameters(index = "2", description = "floor")
+    private int floor;
+    @Override
+    public void run() {
+        System.out.println("Adding room..." + letter+ ", floor: " + floor + " - " + description);
+    }
+}
+@Command(
+        name = "test-api"
+)
+class TestCommand implements Runnable {
+    private static final String apiURL = "http://localhost:8080";
+
+    @Override
+    public void run() {
+        System.out.println("TEST command");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("accept", "application/json")
+                .uri(URI.create(apiURL))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+@Command(name = "multiplex-cli", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Multiplex command-line interface")
+
+class MultiplexCli implements Callable<Integer> {
+
+
+    @Override
+    public Integer call() {
+
+        System.out.println("error");
+        return 0;
+    }
+
+
+}
+
+public class Client {
+    public static void main(String... args) {
+
+        CommandLine cli = new CommandLine(new MultiplexCli());
+        cli.addSubcommand(new AddScreeningRoomCommand());
+        cli.addSubcommand(new AddUserCommand());
+        cli.addSubcommand(new TestCommand());
+        cli.addSubcommand(new ShowUsersCommand());
+
+//        BufferedReader br = null;
+        Scanner scanner = new Scanner(System.in);
+        String cmd;
+        while(true){
+            System.out.println("Enter command (q to quite): ");
+            cmd = scanner.nextLine();
+            if("q".equalsIgnoreCase(cmd)) break;
+            cli.execute(cmd.split(" "));
+            System.out.println(cmd);
+        }
+
+
+    }
+}
---
+++ client/src/main/java/Model/User.java
@@ -0,0 +1,51 @@
+package Model;
+
+
+public class User {
+
+
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    public User() {
+        super();
+    }
+
+
+
+    public User(String firstName, String lastName, String email) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // ------------- GETTERS AND SETTERS ------------- //
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+
+}
