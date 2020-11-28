85f809a8933859a4ebf09c489e268906ac5359c0
PetroS9s <piotrs1999@gmail.com>
2020-11-28 19:04:16 +0100

    base ScreeningRoom structure

--- multiplex/src/main/java/com/example/multiplex/model/ScreeningRoom.java
+++ multiplex/src/main/java/com/example/multiplex/model/ScreeningRoom.java
@@ -1,4 +1,42 @@
 package com.example.multiplex.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ScreeningRoom")
 public class ScreeningRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "Number")
+    private String number;
+
+    @Column(name = "Floor")
+    private Integer floor;
+
+    @Column(name = "Maximal Capacity")
+    private Integer maximalCapacity;
+
+    public ScreeningRoom() { super(); }
+
+    public ScreeningRoom(String number, Integer floor, Integer maximalCapacity){
+        super();
+        this.number = number;
+        this.floor = floor;
+        this.maximalCapacity = maximalCapacity;
+    }
+
+    // ------------- GETTERS AND SETTERS ------------- //
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getNumber() { return number; }
+    public void setNumber(String number) { this.number = number; }
+
+    public Integer getFloor() { return floor; }
+    public void setFloor(Integer floor) { this.floor = floor; }
+
+    public Integer getMaximalCapacity() { return maximalCapacity; }
+    public void setMaximalCapacity(Integer maximalCapacity) { this.maximalCapacity = maximalCapacity; }
 }
