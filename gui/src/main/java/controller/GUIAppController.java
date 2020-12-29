52997c37a37bc39cc8b4eeb397825fee29835648
PetroS9s <piotrs1999@gmail.com>
2020-12-29 17:41:23 +0100

    inter view moving mechanism

--- gui/src/main/java/controller/GUIAppController.java
+++ gui/src/main/java/controller/GUIAppController.java
@@ -9,49 +9,53 @@ import java.io.IOException;
 
 public class GUIAppController {
     private Stage primaryStage;
-    private FXMLLoader loader;
     private GUIController controller;
     public GUIAppController(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.loader = new FXMLLoader();
     }
 
     public void initRootLayout(){
         try{
             this.primaryStage.setTitle("Multiplex");
+            FXMLLoader loader = new FXMLLoader();
             rawStartLayout(loader);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
     }
 
-    private void showScene(BorderPane rootLayout){
+    private void showScene(FXMLLoader loader) throws IOException {
+        BorderPane rootLayout = loader.load();
+        controller = loader.getController();
+        controller.setGuiAppController(this);
         Scene scene = new Scene(rootLayout);
         primaryStage.setScene(scene);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    /*###############################  RAW SECTION    ########################################################*/
 
     public void rawStartLayout(FXMLLoader loader) throws IOException {
-        this.loader.setLocation(GUIAppController.class.getResource("../raw_views/RawStartView.fxml"));
-        BorderPane rootLayout = (BorderPane) this.loader.load();
-        controller = loader.getController();
-        controller.setGuiAppController(this);
-        showScene(rootLayout);
+        loader.setLocation(GUIAppController.class.getResource("../raw_views/RawStartView.fxml"));
+        showScene(loader);
     }
 
     public void rawMoviesLayout() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
         loader.setLocation(GUIAppController.class.getResource("../raw_views/RawMoviesView.fxml"));
-        BorderPane rootLayout = (BorderPane) loader.load();
-        showScene(rootLayout);
+        showScene(loader);
     }
 
     public void rawScreeningsLayout() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
         loader.setLocation(GUIAppController.class.getResource("../raw_views/RawScreeningsView.fxml"));
-        BorderPane rootLayout = (BorderPane) loader.load();
-        showScene(rootLayout);
+        showScene(loader);
     }
 
-    // robimy to layouty dla kazdego pojedyńczego widoku
+    /*###############################  USER SECTION    ########################################################*/
+
+
+
+    /*###############################  ADMIN SECTION    ########################################################*/
 }
--- gui/src/main/java/controller/GUIRawController.java
+++ gui/src/main/java/controller/GUIRawController.java
@@ -3,12 +3,15 @@ package controller;
 import command.CommandRegistry;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class GUIRawController implements GUIController{
     private GUIAppController guiAppController;
+
     private CommandRegistry commandRegistry;
 
     @Override
@@ -33,6 +36,7 @@ public class GUIRawController implements GUIController{
         this.guiAppController.rawScreeningsLayout();
     }
 
+    @FXML
     public void handleGetMoviesOfferAction(javafx.event.ActionEvent actionEvent) throws IOException {
         this.guiAppController.rawMoviesLayout();
     }
